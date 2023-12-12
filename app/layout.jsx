6627795b82099a1f08bfa1438e65f5f238f2bcce
Julie Shapovalenko <jools.shapo@gmail.com>
2023-12-12 11:43:09 +0200

    create auth route and google auth credentials

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,27 @@
-import "@styles/globals.css";
+import '@styles/globals.css';
 
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
-  title: "Share Props Application",
-  description: "Discover & Share AI Prompts",
+  title: 'Share Props Application',
+  description: 'Discover & Share AI Prompts',
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
